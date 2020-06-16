855d66c6abbd85b7fc933334ba6ea25e7b3723c7
Rishabh7080 <60189498+Rishabh7080@users.noreply.github.com>
2020-06-16 21:35:05 +0530

    Add files via upload

---
+++ String.ts
@@ -0,0 +1,17 @@
+/*let msg:String="samsungTV,45000.55,InStock";
+let data:string[]=msg.split(",");
+for(var value of data)
+{
+     console.log(value);
+}*/
+
+ /*let password :string = " jhon";
+if(password.trim()=="jhon")
+{
+    console.log('verifies....');
+}
+else{
+    console.log('Invalid password....');
+}*/
+ 
+
